1b872b522678166d8e9ded2a0b22ecf0fd32b4b1
ChengXiang Qi <18630816527@163.com>
2025-04-07 10:57:23 +0800

    Update leetcode solutions. (#15)
    * add leetcode 394 solution.
    
    * add leetcode 347 solution.
    
    * add leetcode 141 solution.
    
    * add leetcode 2 solution.
    
    * add leetcode 11 solution.
    
    * add leetcode 283 solution.
    
    * add leetcode 226 solution.

--- C++/11-Container-With-Most-Water.cpp
+++ C++/11-Container-With-Most-Water.cpp
@@ -47,3 +47,17 @@ public:
 };
 // @lc code=end
 
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0, r = height.size() -  1;
+        int ans = 0;
+        while(l != r){
+            ans = std::max(ans, (r - l) * std::min(height[l], height[r]));
+            if(height[l] < height[r])l++;
+            else r--;
+        }
+        return ans;
+    }
+};
--- C++/2-Add-Two-Numbers.cpp
+++ C++/2-Add-Two-Numbers.cpp
@@ -52,3 +52,61 @@ public:
         return ans->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* x = l1, *y = l2;
+        ListNode* ans = new ListNode();
+        ListNode* head = ans;
+        int pos = 0;
+        while(x != nullptr || y != nullptr || pos != 0) {
+            if(x == nullptr && y == nullptr){
+                int z = pos;
+                pos = 0;
+                head->val = z;
+            }
+            else if(x == nullptr){
+                int z = (y->val + pos) % 10;
+                pos = (y->val + pos) / 10;
+                head->val = z;
+                y = y->next;
+                if(y != nullptr || pos != 0){
+                    head->next = new ListNode();
+                    head = head->next;
+                }
+            }
+            else if(y == nullptr){
+                int z = (x->val + pos) % 10;
+                pos = (x->val + pos) / 10;
+                head->val = z;
+                x = x->next;
+                if(x != nullptr || pos != 0){
+                    head->next = new ListNode();
+                    head = head->next;
+                }
+            }else{
+                int z = (x->val + y->val + pos) % 10;
+                pos = (x->val + y->val + pos) / 10;
+                head->val = z;
+                x = x->next;
+                y = y->next;
+                if(x != nullptr || y != nullptr || pos != 0){
+                    head->next = new ListNode();
+                    head = head->next;
+                }
+            }
+        }
+        return ans;
+    }
+};
